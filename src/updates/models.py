5d3ff461c2ab09636b0cd80bbc9c52f0f2695917
Kaan Karaca <kaan94karaca@gmail.com>
2019-02-19 00:47:51 +0300

    model created

--- src/updates/models.py
+++ src/updates/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+def upload_update_image(instance, filename):
+    return f'updates/{instance.user}/{filename}'
+
+
+class Update(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to=upload_update_image, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content or ''
