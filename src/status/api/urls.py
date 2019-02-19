b90dd354cd82642a6f9df8992593739479a413ab
Kaan Karaca <kaan94karaca@gmail.com>
2019-02-19 23:55:40 +0300

    Views for CRUDL

--- src/status/api/urls.py
+++ src/status/api/urls.py
@@ -1,22 +1,10 @@
 from django.urls import path
 from .views import (
     StatusAPIView,
-    # StatusListSearchAPIView,
-    # StatusCreateAPIView,
-    # StatusDetailAPIView,
-    # StatusUpdateAPIView,
-    # StatusDeleteAPIView
+    StatusDetailAPIView,
 )
 
 urlpatterns = [
     path('', StatusAPIView.as_view()),
-    # path('create/', StatusCreateAPIView.as_view()),
-    # path('<pk>/', StatusDetailAPIView.as_view()),
-    # path('<pk>/update', StatusUpdateAPIView.as_view()),
-    # path('<pk>/delete', StatusDeleteAPIView.as_view()),
-    # path('', json_example_view),
-    # path('json/cbv', JsonCBV.as_view()),
-    # path('json/cbv2', JsonCBV2.as_view()),
-    # path('json/serialized/list', SerializedListView.as_view()),
-    # path('json/serialized/detail', SerializedDetailView.as_view()),
+    path('<pk>/', StatusDetailAPIView.as_view()),
 ]
