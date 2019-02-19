a8d3f3f97fdf6285b0d6112e2e5da671edb44b4e
Kaan Karaca <kaan94karaca@gmail.com>
2019-02-19 23:01:31 +0300

    One API Endpoint for CRUDL

--- src/status/api/views.py
+++ src/status/api/views.py
@@ -1,9 +1,19 @@
+import json
 from rest_framework import generics, mixins
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from status.models import Status
 from .serializers import StatusSerializer
+
+
+def is_json(json_data):
+    try:
+        real_json = json.loads(json_data)
+        is_valid = True
+    except ValueError:
+        is_valid = False
+    return is_valid
 
 
 # CreateModelMixin -- POST data
@@ -18,6 +28,7 @@ class StatusAPIView(
     permission_classes = []
     authentication_classes = []
     serializer_class = StatusSerializer
+    passed_id = None
 
     def get_queryset(self):
         request = self.request
@@ -29,7 +40,7 @@ class StatusAPIView(
 
     def get_object(self):
         request = self.request
-        passed_id = request.GET.get('id', None)
+        passed_id = request.GET.get('id', None) or self.passed_id
         queryset = self.get_queryset()
         obj = None
         if passed_id is not None:
@@ -37,8 +48,21 @@ class StatusAPIView(
             self.check_object_permissions(request, object)
         return obj
 
+    def perform_destroy(self, instance):
+        if instance is not None:
+            return instance.delete()
+        return None
+
     def get(self, request, *args, **kwargs):
-        passed_id = request.GET.get('id', None)
+        url_passed_id = request.GET.get('id', None)
+        json_data = {}
+        body_ = request.body
+        if is_json(body_):
+            json_data = json.loads(request.body)
+        new_passed_id = json_data.get('id', None)
+
+        passed_id = url_passed_id or new_passed_id or None
+        self.passed_id = passed_id
         if passed_id is not None:
             return self.retrieve(request, *args, **kwargs)
         return super(StatusAPIView, self).get(request, *args, **kwargs)
@@ -47,12 +71,39 @@ class StatusAPIView(
         return self.create(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
+        url_passed_id = request.GET.get('id', None)
+        json_data = {}
+        body_ = request.body
+        if is_json(body_):
+            json_data = json.loads(request.body)
+        new_passed_id = json_data.get('id', None)
+
+        passed_id = url_passed_id or new_passed_id or None
+        self.passed_id = passed_id
         return self.update(request, *args, **kwargs)
 
     def patch(self, request, *args, **kwargs):
+        url_passed_id = request.GET.get('id', None)
+        json_data = {}
+        body_ = request.body
+        if is_json(body_):
+            json_data = json.loads(request.body)
+        new_passed_id = json_data.get('id', None)
+
+        passed_id = url_passed_id or new_passed_id or None
+        self.passed_id = passed_id
         return self.update(request, *args, **kwargs)
 
     def delete(self, request, *args, **kwargs):
+        url_passed_id = request.GET.get('id', None)
+        json_data = {}
+        body_ = request.body
+        if is_json(body_):
+            json_data = json.loads(request.body)
+        new_passed_id = json_data.get('id', None)
+
+        passed_id = url_passed_id or new_passed_id or None
+        self.passed_id = passed_id
         return self.destroy(request, *args, **kwargs)
 
 #
