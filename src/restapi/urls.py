995a1e7f400f20d8d6b559ff58cdec701d6fd7d0
Kaan Karaca <kaan94karaca@gmail.com>
2019-02-20 01:38:53 +0300

    Implement JWT Authentication

--- src/restapi/urls.py
+++ src/restapi/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from updates.views import json_example_view, JsonCBV, JsonCBV2, SerializedListView, SerializedDetailView
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/status/', include('status.api.urls')),
     path('api/updates/', include('updates.api.urls')),
-    # path('', json_example_view),
-    # path('json/cbv', JsonCBV.as_view()),
-    # path('json/cbv2', JsonCBV2.as_view()),
-    # path('json/serialized/list', SerializedListView.as_view()),
-    # path('json/serialized/detail', SerializedDetailView.as_view()),
+    path('api/auth/jwt', obtain_jwt_token),
+    path('api/auth/jwt/refresh/', refresh_jwt_token),
 ]
