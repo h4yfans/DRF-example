35414db9e0fb0edfc9c84cf2d70e0a30d80d8e5c
Kaan Karaca <kaan94karaca@gmail.com>
2019-02-20 12:20:34 +0300

    Custom Permissions

--- src/accounts/api/views.py
+++ src/accounts/api/views.py
@@ -6,6 +6,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_jwt.settings import api_settings
 
+from .permissions import AnonPermissionOnly
 from .serializers import UserRegisterSerializer
 
 jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
@@ -15,7 +16,7 @@ User = get_user_model()
 
 
 class AuthAPIView(APIView):
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [AnonPermissionOnly]
 
     def post(self, request, *args, **kwargs):
         if request.user.is_authenticated:
@@ -40,7 +41,7 @@ class AuthAPIView(APIView):
 class RegisterAPIView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegisterSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [AnonPermissionOnly]
 
     def get_serializer_context(self):
         return {'request': self.request}
